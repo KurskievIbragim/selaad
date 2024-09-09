9903f73f8ba1ceae0825fec992bf29c8fbbb7ff5
KurskievIbragim <124819924+KurskievIbragim@users.noreply.github.com>
2024-09-09 12:23:28 +0300

    Update StoreController.php

--- app/Http/Controllers/Admin/Republic/StoreController.php
+++ app/Http/Controllers/Admin/Republic/StoreController.php
@@ -11,46 +11,34 @@ use Illuminate\Support\Facades\Storage;
 class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
-    {
+{
+    $data = $request->validated();
 
-        $data = $request->validated();
+    $slides = $request->file('slides');
+    $slidesArray = [];
 
-
-
-        $slides = $request->file('slides');
-        $slidesString = '';
-
-        if ($slides) {
-            $slideNames = [];
-
-            foreach ($slides as $slide) {
-                $filename = $slide->store('slide_images'); // Save the slide image and get the filename
-                $slideNames[] = $filename; // Add the filename to the array
-            }
-
-            $slidesString = implode(',', $slideNames); // Соединяем имена файлов через запятую
+    if ($slides) {
+        foreach ($slides as $slide) {
+            $filename = $slide->store('slide_images'); // Save the slide image and get the filename
+            $slidesArray[] = $filename; // Add the filename to the array
         }
-
-        if ($request->hasFile('image_main')) {
-            $imageMain = $request->file('image_main');
-            $imageMainPath = $imageMain->store('images'); // Store the image in the 'images' folder
-
-            $data['image_main'] = $imageMainPath;
-        }
-
-
-        KnowRepublic::create([
-            'title' => $request->title,
-            'type' => $request->type,
-            'content' => $request->content,
-            'image_main' => $data['image_main'],
-            'slides' => $slidesString, // Сохраняем имена файлов как строку
-        ]);
-
-
-
-
-
-       return redirect()->route('admin.republic.index');
     }
+
+    if ($request->hasFile('image_main')) {
+        $imageMain = $request->file('image_main');
+        $imageMainPath = $imageMain->store('images'); // Store the image in the 'images' folder
+        $data['image_main'] = $imageMainPath;
+    }
+
+    KnowRepublic::create([
+        'title' => $request->title,
+        'type' => $request->type,
+        'content' => $request->content,
+        'image_main' => $data['image_main'],
+        'slides' => json_encode($slidesArray), // Сохраняем имена файлов как JSON
+    ]);
+
+    return redirect()->route('admin.republic.index');
+}
+
 }
